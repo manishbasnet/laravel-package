4daf2de1a7a7eb68356224d8946c41981805a11b
GrowByData <manish.basnetnp@gmail.com>
2019-04-21 20:42:17 +0545

    new package

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Waygou\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+		dd("We are loading our package service provider");
+	}
+
+	public function register()
+	{
+
+	}
+}
